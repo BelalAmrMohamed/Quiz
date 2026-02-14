6098a34957ba4fbf75af70c6f141c5fb9d09bddf
BelalAmrMohamed <belalamrofficial@gmail.com>
2026-02-14 17:25:36 +0200

    Updating

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // Service Worker for Basmagi Quiz Platform
 // Provides offline support, caching, and performance improvements
 
-const CACHE_VERSION = "basmagi-v2.2.0";
+const CACHE_VERSION = "basmagi-v2.3.0";
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const DYNAMIC_CACHE = `${CACHE_VERSION}-dynamic`;
 const IMAGE_CACHE = `${CACHE_VERSION}-images`;
